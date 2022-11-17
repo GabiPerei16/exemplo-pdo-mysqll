8c1aa45682e40766800f770708e435338a08a2ee
Gabippeira <Gp18997543977@gmail.com>
2022-11-17 13:13:46 +0000

    Preparação da Base de Dados

---
+++ assets/dados_iniciais.sql
@@ -0,0 +1,11 @@
+INSERT INTO generos(id,nome) values
+(1, 'Alta Fantasia'),
+(2, 'Policial'),
+(3, 'Drama'),
+(4, 'Ficção Cientifica');
+
+INSERT INTO livros(id, titulo, id_genero) values
+(1, 'Assassinato no Expresso do Oriente', 2),
+(2, 'A sociedade do Anel', 1),
+(3, 'Como eu Era Antes de Voce', 3),
+(4, 'O Guia do Mochileiro das Galaxias', 4);
