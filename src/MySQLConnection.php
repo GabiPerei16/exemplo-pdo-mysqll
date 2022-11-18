65ec2adb64a5b86956ab278b71242254ed1f9ccd
Gabippeira <Gp18997543977@gmail.com>
2022-11-18 12:13:56 +0000

    Connection Class

---
+++ src/MySQLConnection.php
@@ -0,0 +1,9 @@
+<?php
+namespace ExemploPDOMySQL;
+
+class MySQLConnection extends \PDO {
+    public function _construct() {
+        parent::_construct('mysql:host=localhost;dbname=biblioteca', 'root', '');
+        
+    }
+}
